a269812081e1a28cee94549d78f4ae8a877dde67
Hadeel Daragmeh <hadeel.daragmeh@taqneensoft.com>
2026-03-06 01:05:01 +0300

    add hello controller and update pom

---
+++ src/main/java/com/sp/secu/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.sp.secu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring Security works!";
+    }
+}
