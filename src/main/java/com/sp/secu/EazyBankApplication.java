bea938a31ca1f27841fe36af24312ce2e451f042
Hadeel Daragmeh <hadeel.daragmeh@taqneensoft.com>
2026-03-06 02:58:42 +0300

    Fix project

--- src/main/java/com/sp/secu/EazyBankApplication.java
+++ src/main/java/com/sp/secu/EazyBankApplication.java
@@ -2,12 +2,14 @@ package com.sp.secu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class SecuApplication {
+@ComponentScan(basePackages = "com.sp")
+public class EazyBankApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SecuApplication.class, args);
+        SpringApplication.run(EazyBankApplication.class, args);
     }
 
 }
--- src/main/java/com/sp/secu/controller/HomeController.java
+++ src/main/java/com/sp/secu/controller/HomeController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/welcome")
     public String home() {
-        return "Spring Security works!";
+        return "Welcome, it just easy application with security !";
     }
 }
